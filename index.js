6f1f9433bf060b3e50217b1f2eb85768fa84acfb
Gourab <gourabg998@gmail.com>
2021-10-31 01:14:21 +0600

    review

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
-const res = require("express/lib/response");
+const ObjectId = require("mongodb").ObjectId;
+
 require("dotenv").config();
 
 const app = express();
@@ -28,6 +29,7 @@ async function run() {
     const database = client.db("tourde");
     const packageCollection = database.collection("package");
     const bookingCollection = database.collection("booking");
+    const reviewCollection = database.collection("review");
 
     //packageGet
     app.get("/package", async (req, res) => {
@@ -41,6 +43,40 @@ async function run() {
       const cursor = bookingCollection.find({});
       const booking = await cursor.toArray();
       res.send(booking);
+    });
+    //reviewGet
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+
+    //deleteBooking
+
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingCollection.deleteOne(query);
+      res.send(booking);
+    });
+
+    //bookingUpdate
+    app.put("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateBooking = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: "approve",
+        },
+      };
+      const result = await bookingCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
     });
 
     //packagePost
@@ -56,6 +92,14 @@ async function run() {
     app.post("/createBooking", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollection.insertOne(booking);
+      console.log(`Booking created with id: ${result.insertedId}`);
+      res.json(result);
+    });
+
+    //reviewPost
+    app.post("/createReview", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
       console.log(`Booking created with id: ${result.insertedId}`);
       res.json(result);
     });
